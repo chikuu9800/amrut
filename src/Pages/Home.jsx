30e65cc51787f8130a0044366f8070767417ff28
chikuu9800 <shubhamchikhale40@gmail.com>
2025-11-06 19:55:10 +0530

    header changes 3

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,8 @@ import Tabs from '../Components/Tabs'
 import AnimatedMap from '../Components/AnimatedMap'
 import MaharashtraShowcase from '../Components/SliderMaha'
 import Footer from '../Components/Footer'
+import AmrutStats from '../Components/Dynamic'
+import AmrutMissionVision from '../Components/Vison'
 // import YojanaSlider from '../Components/Slider'
 
 
@@ -12,10 +14,12 @@ const Home = () => {
   return (
     <div className='overflow-hidden' >
       <EmbassyBanner/>
+      <AmrutStats/>
       <SLogon/>
       <Tabs/>
       {/* <YojanaSlider/> */}
       <AnimatedMap/>
+      <AmrutMissionVision/>
       <MaharashtraShowcase/>
       <Footer/>
     </div>
