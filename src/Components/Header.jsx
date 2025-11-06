9312c6ed519255b0c3d9c0f1c8af55fc3247f584
chikuu9800 <shubhamchikhale40@gmail.com>
2025-11-06 18:50:10 +0530

    header changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -113,7 +113,7 @@ export default function Header() {
 
     const renderMobileMenu = () => (
         <div className="lg:hidden">
-            <div className="flex items-center justify-between px-4 py-3">
+            <div className="flex items-center justify-between px-4 py-3 w-full ">
                 <span className="text-[#ff671f] text-base">मेनू</span>
                 <button
                     onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
